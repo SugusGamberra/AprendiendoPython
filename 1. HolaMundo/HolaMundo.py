e38e540854001bec45ecf0034d58677575bec673
SugusGamberra <125316695+SugusGamberra@users.noreply.github.com>
2025-10-06 16:38:07 +0200

    Add files via upload
    Reorganizacion, adicion de la calculadora del area de un triangulo

---
+++ 1. HolaMundo/HolaMundo.py
@@ -0,0 +1,6 @@
+# "Hello World!"
+# Print es para mostrar lo que hagas en la consola
+# Cualquier texto entre comillas es de tipo String
+
+print("Hola mundo!")
+print("Este es nuestro primer programa en Python!")
---
+++ 2. CalculadoraAreaTriangulo/main.py
@@ -0,0 +1,22 @@
+# Calculo del area de un triangulo
+# Triangulo 1:
+
+base = 10
+altura = 5
+
+area = (base * altura) / 2
+
+print("Triángulo 1: ")
+print("La base es: ", base)
+print("La altura es: ", altura)
+print("Por ende, el área del triángulo es: ", area)
+
+# Triangulo 2:
+
+base = 25
+altura = 40
+
+area = (base * altura) / 2
+
+print("Triangulo 2: ")
+print("El resultado es: ", area)
